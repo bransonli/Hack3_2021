90b6eeb6eec1bc5bd92c5086c43a4e4b9f9278b6
aaron li <bransonchen1014@gmail.com>
2021-06-27 14:27:12 +0800

    using video

---
+++ video_view.py
@@ -0,0 +1,62 @@
+#dependencies
+from tensorflow import keras
+import tensorflow as tf
+from keras.models import load_model
+import matplotlib.pyplot as plt
+from matplotlib import image
+import numpy as np
+from PIL import Image
+import cv2
+
+#local scripts
+import main
+import time
+
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture(r'C:\Users\aaron\Pictures\Camera Roll\test_video.mp4')
+face_cascade = cv2.CascadeClassifier('frontal_face.xml')
+
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+    print("Error opening video stream or file")
+
+# Read until video is completed
+while(cap.isOpened()):
+    # Capture frame-by-frame
+    ret, img = cap.read()
+    img = cv2.resize(img,(224,224),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    if ret == True:
+        initial = img
+
+        # Display the resulting frame
+        img = cv2.putText(img, main.main(img, 'live'), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        #cv2.imshow('Frame',img)
+
+        print(initial == img)
+
+
+    
+
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+        # Break the loop
+    else: 
+        break
+
+# When everything done, release the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
+
