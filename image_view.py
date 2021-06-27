264ae9057ef7afb3f808b76e0c93038ed7e11d29
aaron li <bransonchen1014@gmail.com>
2021-06-27 14:26:30 +0800

    using image

---
+++ image_view.py
@@ -0,0 +1,48 @@
+#dependencies
+from tensorflow import keras
+import tensorflow as tf
+from keras.models import load_model
+import matplotlib.pyplot as plt
+from matplotlib import image
+import numpy as np
+from PIL import Image
+import cv2
+
+#local scripts
+import main
+import time
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('frontal_face.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(r"C:\Users\aaron\Desktop\incorrect1.jpg")
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+
+
+# Read the frame
+ret, img = cap.read()
+img = cv2.resize(img,(224,224),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+# Convert to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Detect the faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+# Draw the rectangle around each face
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+# Display
+
+#cv2.putText(main.main(img, 'live'))
+cv2.putText(img, main.main(img, 'live'), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+    
+while True:
+    # Stop if escape key is pressed
+    cv2.imshow('img', img)
+    k = cv2.waitKey(1) & 0xff
+    if k==27:
+        break
+
+    
+# Release the VideoCapture object
+cap.release()
